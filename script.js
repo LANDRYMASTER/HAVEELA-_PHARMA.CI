412350c6df0cd9bf95f6419f4b35a3eb8e5fd03d
LANDRYMASTER <Angebile146@gmail.com>
2025-05-08 17:14:58 +0200

    VERIFICATION

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-    const btn = document.getElementById('menu-btn');
-    const menu = document.getElementById('menu');
-    const menuOpenIcon = document.getElementById('menu-open');
-    const menuCloseIcon = document.getElementById('menu-close');
-    const image = document.querySelector('#image_hero');
-    const section = document.getElementById('hero');
-    const titre = document.getElementById('infosup');
-    const Img_propos = document.querySelector('.Imgpropos');
-    const images = ['cover4','cover2','cover3','cover1'];
-    const bubble = document.getElementById("contact-bubble");
-    const form = document.getElementById("contact-form");
-    const closeBtn = document.getElementById("close-btn");
-    const links = document.querySelectorAll('.nav-link');
-    const currentPage = window.location.pathname.split('/').pop();
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const menuOpenIcon = document.getElementById('menu-open');
+const menuCloseIcon = document.getElementById('menu-close');
+const image = document.querySelector('#image_hero');
+const section = document.getElementById('hero');
+const Img_propos = document.querySelector('.Imgpropos');
+const images = ['cover4','cover2','cover3','cover1'];
+const bubble = document.getElementById("contact-bubble");
+const form = document.getElementById("contact-form");
+const closeBtn = document.getElementById("close-btn");
+const links = document.querySelectorAll('.nav-link');
+const nav = document.querySelector('nav');
+const container = document.getElementById('bubble-container');
 
-    let index = 0;
-    let index1 = 1;
-    let isOpen = false;
+let index = 0;
+let isOpen = false;
 
-    
-    links.forEach(link => {
-      if (link.getAttribute('href') === currentPage) {
-        link.classList.add('active');
-      }
-    });
 
 window.addEventListener('scroll', function () {
     if (window.scrollY >=180 &&  window.scrollY <= 1300) {
         Img_propos.classList.remove('Imgpropos');
     }else {
         Img_propos.classList.add('Imgpropos');
+    }
+
+    if (window.scrollY >= 50 ) {
+      nav.classList.remove('bg-[#4a86b0]');
+      nav.classList.add('bg-[#4a85b099]');
+    }
+    else {
+      nav.classList.remove('bg-[#4a85b099]');
+      nav.classList.add('bg-[#4a86b0]');
     }
 });
 
@@ -46,33 +48,23 @@ btn.addEventListener('click', () => {
     }
 });
 
+function afficherHeure() {
+    const maintenant = new Date();
+    const heure = maintenant.toLocaleTimeString();
+    document.querySelector('#heure').textContent = heure;
+  }
+
+setInterval(afficherHeure, 1000);
+afficherHeure();
+
 function changerImage() {
         section.classList.remove(section.getAttribute("class"));
         section.classList.add(images[index]);
         index = (index + 1) % images.length;
-
-        switch (index1) {
-            case 1:
-                titre.innerHTML = 'HAVEELA pharma <br> votre levier de croissance';
-                index1 += 1;
-                break;
-            case 2:
-                titre.innerHTML = 'Maitrise de la <br> chaine approvisionnement des produits';
-                index1 += 1;
-                break;
-            case 3:
-                titre.innerHTML = 'HAVEELA pharma <br> votre levier de croissance';
-                index1 += 1;
-                break;
-            case 4:
-                titre.innerHTML = 'Nous vous offrons <br> des solutions sur mesure';
-                index1 = 1;
-                break;
-        }
 }
 
 // Changer d'image toutes les 5 secondes
-setInterval(changerImage, 4000);
+setInterval(changerImage, 2500);
 
 function openForm() {
     form.classList.remove("opacity-0", "scale-95", "pointer-events-none");
@@ -93,3 +85,22 @@ function openForm() {
   });
 
   closeBtn.addEventListener("click", closeForm);
+
+function createBubble() {
+  const bubble = document.createElement('div');
+  bubble.classList.add('bubble');
+  const size = Math.random() * 20 + 10;
+  bubble.style.width = `${size}px`;
+  bubble.style.height = `${size}px`;
+  bubble.style.left = `${Math.random() * window.innerWidth}px`;
+  bubble.style.bottom = `0px`;
+  container.appendChild(bubble);
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 5000);
+}
+
+// Créer une bulle toutes les 300ms
+setInterval(createBubble, 500);
+
